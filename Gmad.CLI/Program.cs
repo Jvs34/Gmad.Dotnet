c6075a994ad10e19f00e4e1ce1f44c75379161e7
Jvs <jvstheluacoder@gmail.com>
2020-02-04 22:40:49 +0100

    Wip commands

--- Gmad.CLI/Program.cs
+++ Gmad.CLI/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.CommandLine;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Gmad.CLI
 {
 	internal static class Program
 	{
-		private static void Main( string [] args )
+		private static async Task<int> Main( string [] args )
 		{
-			Console.WriteLine( "Hello World!" );
+			var createCommand = new Command( "create" )
+			{
+				new Option( "-folder", "the folder to turn into an addon" )
+				{
+					Argument = new Argument<DirectoryInfo>(),
+				},
+				new Option( "-out" , "the file output ending in .gma" )
+				{
+					Argument = new Argument<FileInfo>()
+				},
+				new Option( "-warninvalid" , "automatically skip invalid files" )
+			};
+
+			var extractCommand = new Command( "extract" )
+			{
+
+			};
+
+
+			var rootCommand = new RootCommand();
+
+			rootCommand.AddCommand( createCommand );
+			rootCommand.AddCommand( extractCommand );
+
+			return await rootCommand.InvokeAsync( args );
 		}
 	}
 }
---
+++ Gmad.Shared/Create.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gmad.Shared
+{
+	class Create
+	{
+	}
+}
